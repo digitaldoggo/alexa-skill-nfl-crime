ce2dda5147c1fbc7a883b509ecccf28dd874fb10
k3yb0ardn1nja <benmhedrick@gmail.com>
2016-05-21 15:32:05 -0400

    Added HelpIntent, YesIntent, and NoIntent handlers.

--- index.js
+++ index.js
@@ -20,9 +20,15 @@ app.launch(function(req,res) {
 });
 
 app.pre = function(request,response,type) {
-	console.log("Pre method reached with type: " + type);
+	var intent;
+	if (request.data &&
+		request.data.request &&
+		request.data.request.intent) {
+		intent = request.data.request.intent.name;
+	}
 	
-	// TODO: update applicatoinId
+	console.log("Pre method reached with type: " + type + " and intent: " + intent);
+	
     if (request.sessionDetails.application.applicationId!="amzn1.echo-sdk-ams.app.4a97dc92-2470-4080-a7bb-2667f3eb84f2") {
         // Fail ungracefully
         response.fail("Invalid applicationId");
@@ -44,113 +50,208 @@ app.error = function(exception, request, response) {
     response.say("Sorry, an error occurred.");
 };
 
-app.intent('RandomCrimeIntent',{
-		"slots":{"ANSWER":"LITERAL"}
-		,"utterances":[
-			"{yes|no|ANSWER}",
-			"{for|to get} a {random crime|crime}",
-			"to tell me a {random crime|crime}"
-		]
-	},
-	function(req,res) {
-		console.log("RandomCrimeIntent entered");
+app.intent('AMAZON.HelpIntent',{}, function(request, response) {
+	var prompt = "I can provide you with random NFL player crime facts. You can say, ask Football Crime for a random crime fact. Would you like to get a random NFL player crime fact?";
+	var reprompt = "Would you like to get a random NFL player crime fact?";
+	response.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+});
+
+app.intent('AMAZON.NoIntent',{}, function(request, response) {
+	response.send();
+});
+
+app.intent('AMAZON.YesIntent',{}, function(req, res) {
+	console.log("AMAZON.YesIntent entered");
+	
+	var options = {
+		host: 'nflarrest.com',
+		path: '/api/v1/player'
+	};
+
+	http.request(options, function(response) {
+		var playerData = '';
 		
-		var answer = req.slot('ANSWER');
-		
-		if((typeof answer === 'undefined' && answer == null) || answer === "yes") {
-		
+		response.on('data', function (chunk) {
+			playerData += chunk;
+		});
+
+		//the whole response has been recieved, so we just print it out here
+		response.on('end', function () {
+			playerData = JSON.parse(playerData);
+			
+			// get random player name
+			var randomPlayer = playerData[random.getRandomInt(0, playerData.length - 1)];
+			
+			console.log(randomPlayer.Name);
+			
 			var options = {
 				host: 'nflarrest.com',
-				path: '/api/v1/player'
+				path: '/api/v1/player/arrests/' + encodeURI(randomPlayer.Name)
 			};
 
 			http.request(options, function(response) {
-				var playerData = '';
+				var arrestData = '';
 				
 				response.on('data', function (chunk) {
-					playerData += chunk;
+					arrestData += chunk;
 				});
 
 				//the whole response has been recieved, so we just print it out here
 				response.on('end', function () {
-					playerData = JSON.parse(playerData);
+					arrestData = JSON.parse(arrestData);
 					
-					// get random player name
-					var randomPlayer = playerData[random.getRandomInt(0, playerData.length - 1)];
+					// get random arrest record
+					var randomArrest = arrestData[random.getRandomInt(0, arrestData.length - 1)];
 					
-					console.log(randomPlayer.Name);
+					console.log(JSON.stringify(randomArrest));
 					
-					var options = {
-						host: 'nflarrest.com',
-						path: '/api/v1/player/arrests/' + encodeURI(randomPlayer.Name)
-					};
-
-					http.request(options, function(response) {
-						var arrestData = '';
+					if(randomArrest.Date && randomPlayer.Name) {
 						
-						response.on('data', function (chunk) {
-							arrestData += chunk;
-						});
-
-						//the whole response has been recieved, so we just print it out here
-						response.on('end', function () {
-							arrestData = JSON.parse(arrestData);
-							
-							// get random arrest record
-							var randomArrest = arrestData[random.getRandomInt(0, arrestData.length - 1)];
-							
-							console.log(JSON.stringify(randomArrest));
-							
-							if(randomArrest.Date && randomPlayer.Name) {
-								
-								var dateToFormat = new Date(randomArrest.Date);
-								var day = dateToFormat.getDate(); // prevent leading zero
-								randomArrest.Date = strftime("%B " + day +", %Y", dateToFormat);
-							
-								// TODO: remove unnecessary commas to prevent strange breaks
-								var speech = "On " + randomArrest.Date + ", " + randomPlayer.Name;
-								
-								if(positions[randomArrest.Position]) {
-									if(randomArrest.Team === "free") {
-										speech += ", a free agent " + positions[randomArrest.Position];
-										speech += ", ";
-									}
-									else {
-										speech += ", a " + positions[randomArrest.Position];
-										if(!teams[randomArrest.Team]) {
-											speech += ", ";
-										}
-									}
-									
-								}
-								if(teams[randomArrest.Team]) {
-									speech += " of the " + teams[randomArrest.Team];
+						var dateToFormat = new Date(randomArrest.Date);
+						var day = dateToFormat.getDate(); // prevent leading zero
+						randomArrest.Date = strftime("%B " + day +", %Y", dateToFormat);
+					
+						// TODO: remove unnecessary commas to prevent strange breaks
+						var speech = "On " + randomArrest.Date + ", " + randomPlayer.Name;
+						
+						if(positions[randomArrest.Position]) {
+							if(randomArrest.Team === "free") {
+								speech += ", a free agent " + positions[randomArrest.Position];
+								speech += ", ";
+							}
+							else {
+								speech += ", a " + positions[randomArrest.Position];
+								if(!teams[randomArrest.Team]) {
 									speech += ", ";
 								}
-								speech += " was " + randomArrest.Encounter + " for " + randomArrest.Category;
-								if(randomArrest.Description) {
-									speech += "... " + randomArrest.Description;
-								}
-								else {
-									speech += ".";
-								}
-							
 							}
-							res.say(speech);
-							res.card("Random Crime Record", speech);
-							res.send();
-						});
-					}).end();
+							
+						}
+						if(teams[randomArrest.Team]) {
+							speech += " of the " + teams[randomArrest.Team];
+							speech += ", ";
+						}
+						speech += " was " + randomArrest.Encounter + " for " + randomArrest.Category;
+						if(randomArrest.Description) {
+							speech += "... " + randomArrest.Description;
+						}
+						else {
+							speech += ".";
+						}
+					
+					}
+					res.say(speech);
+					res.card("Random Crime Record", speech);
+					res.send();
 				});
 			}).end();
+		});
+	}).end();
+	
+	return false;
+});
+
+app.intent('RandomCrimeIntent',{
+		"slots": {}
+		,"utterances":[
+			"{for|to get} a {random crime|crime}",
+			"to tell me a {random crime|crime}"
+		]
+	},
+	function(req, res) {
+		console.log("RandomCrimeIntent entered");
+		
+		var options = {
+			host: 'nflarrest.com',
+			path: '/api/v1/player'
+		};
+
+		http.request(options, function(response) {
+			var playerData = '';
 			
-			return false;
-		}
+			response.on('data', function (chunk) {
+				playerData += chunk;
+			});
+
+			//the whole response has been recieved, so we just print it out here
+			response.on('end', function () {
+				playerData = JSON.parse(playerData);
+				
+				// get random player name
+				var randomPlayer = playerData[random.getRandomInt(0, playerData.length - 1)];
+				
+				console.log(randomPlayer.Name);
+				
+				var options = {
+					host: 'nflarrest.com',
+					path: '/api/v1/player/arrests/' + encodeURI(randomPlayer.Name)
+				};
+
+				http.request(options, function(response) {
+					var arrestData = '';
+					
+					response.on('data', function (chunk) {
+						arrestData += chunk;
+					});
+
+					//the whole response has been recieved, so we just print it out here
+					response.on('end', function () {
+						arrestData = JSON.parse(arrestData);
+						
+						// get random arrest record
+						var randomArrest = arrestData[random.getRandomInt(0, arrestData.length - 1)];
+						
+						console.log(JSON.stringify(randomArrest));
+						
+						if(randomArrest.Date && randomPlayer.Name) {
+							
+							var dateToFormat = new Date(randomArrest.Date);
+							var day = dateToFormat.getDate(); // prevent leading zero
+							randomArrest.Date = strftime("%B " + day +", %Y", dateToFormat);
+						
+							// TODO: remove unnecessary commas to prevent strange breaks
+							var speech = "On " + randomArrest.Date + ", " + randomPlayer.Name;
+							
+							if(positions[randomArrest.Position]) {
+								if(randomArrest.Team === "free") {
+									speech += ", a free agent " + positions[randomArrest.Position];
+									speech += ", ";
+								}
+								else {
+									speech += ", a " + positions[randomArrest.Position];
+									if(!teams[randomArrest.Team]) {
+										speech += ", ";
+									}
+								}
+								
+							}
+							if(teams[randomArrest.Team]) {
+								speech += " of the " + teams[randomArrest.Team];
+								speech += ", ";
+							}
+							speech += " was " + randomArrest.Encounter + " for " + randomArrest.Category;
+							if(randomArrest.Description) {
+								speech += "... " + randomArrest.Description;
+							}
+							else {
+								speech += ".";
+							}
+						
+						}
+						res.say(speech);
+						res.card("Random Crime Record", speech);
+						res.send();
+					});
+				}).end();
+			});
+		}).end();
+		
+		return false;
 	}
 );
 
 // Use for deploying to AWS Lambda
-//module.exports.handler = app.lambda();
+module.exports.handler = app.lambda();
 
 // Use for local testing
-module.exports = app;
+//module.exports = app;
